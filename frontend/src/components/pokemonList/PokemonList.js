7ecbadd553446a5ae9c20313afc47d78fdff2fbd
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-10-20 14:19:13 +0200

    add image to PokemonList.js

--- frontend/src/components/pokemonList/PokemonList.js
+++ frontend/src/components/pokemonList/PokemonList.js
@@ -4,7 +4,10 @@ export default function PokemonList({ pokeList }) {
     return (
         <>
             {pokeList.map(p => (
-                <div key={p.name}>{p.name}</div>
+                <div key={p.name}>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt=""/>
+                    <span>{p.name}</span>
+                </div>
             )
             )}
         </>
