c2ca61b93f8fbe5f6fed637ff925bd8f781e749d
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-02 12:55:26 +0100

    make pokemon id's 3 digits long

--- frontend/src/components/pokemonList/PokemonListItem.js
+++ frontend/src/components/pokemonList/PokemonListItem.js
@@ -1,13 +1,14 @@
 import "./PokemonList.css";
 
-export default function PokemonList({ pokeList, color }) {
+export default function PokemonListItem({ pokeList}) {
+
     return (
         <>
             <div className="card-container">
             {pokeList.map(p => (
-                <div style={{backgroundColor: color }} className="card" key={p.name}>
+                <div style={{backgroundColor: "white" }} className="card" key={p.name}>
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt=""/>
-                    <div>{p.id}</div>
+                    <div>#{p.id.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})}</div>
                     <div>{p.name.toUpperCase()}</div>
                 </div>
             )
