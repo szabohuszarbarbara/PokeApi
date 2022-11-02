2ba6eba56d156b768a0873f88fb4a4f01d739984
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-02 12:57:09 +0100

    add file for Pagination and two buttons

---
+++ frontend/src/components/pagination/Pagination.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Pagination(props) {
+    return (
+        <div>
+            <button>Previous</button>
+            <button>Next</button>
+
+        </div>
+    );
+}
+
+export default Pagination;
