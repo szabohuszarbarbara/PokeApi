3bfbc61c299543914d7ad948967757c9470381e0
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-02 16:10:13 +0100

    add modal show

--- frontend/src/components/pokemonList/PokemonListItem.js
+++ frontend/src/components/pokemonList/PokemonListItem.js
@@ -1,18 +1,28 @@
 import "./PokemonList.css";
+import PokemonData from "../pokemonData/PokemonData";
+import {useState} from "react";
 
 export default function PokemonListItem({ pokeList}) {
+    const [show, setShow] = useState(false);
+    const [id, setId] = useState(0);
+
+    const openModal = (num) => {
+        setShow(true);
+        setId( num);
+    }
 
     return (
         <>
             <div className="card-container">
             {pokeList.map(p => (
-                <div style={{backgroundColor: "white" }} className="card" key={p.name}>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt=""/>
-                    <div>#{p.id.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})}</div>
-                    <div>{p.name.toUpperCase()}</div>
+                <div style={{backgroundColor: "white" }} className="card" key={p.name} onClick={ () => openModal(p.id) }>
+                    <div className={"card-image"}><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt=""/></div>
+                    <div className={"card-id"}>#{p.id.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})}</div>
+                    <div className={"card-name"}>{p.name.toUpperCase()}</div>
                 </div>
             )
             )}
+                <PokemonData show={ show } id = {id} />
             </div>
         </>
     )
