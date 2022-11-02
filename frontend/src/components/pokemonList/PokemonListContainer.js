3e78ddc18ceabfa1b675822690d370386cfde799
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-02 12:56:34 +0100

    refactor useEffect to call a getData method

--- frontend/src/components/pokemonList/PokemonListContainer.js
+++ frontend/src/components/pokemonList/PokemonListContainer.js
@@ -1,35 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import PokemonList from "./PokemonList";
+import PokemonListItem from "./PokemonListItem";
+import Pagination from "../pagination/Pagination";
 
 function PokemonListContainer() {
     const [pokeList, setPokeList] = useState([]);
-    const [color, setColor] = useState("white")
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon")
 
     useEffect(  () => {
-        axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-            getPokeList(res.data.results.map(p => p.url));
-        })
+        getData();
     }, [])
 
-    const getPokeList = (url) => {
+    const getData = async () => {
+        await axios.get(url).then(result => {
+            const resultList = result.data.results
+            const urls = resultList.map(p => p.url)
+            getPokeList(urls);
+        })
+            .catch(function (error) {
+                console.log(error);
+            })
 
-        url.map(p => axios.get(p).then(result => {
+    }
+
+    const getPokeList = async (urls) => {
+        urls.map( async url => await axios.get(url).then( async result => {
+
+/*            await getPokeColor(result.data.species.url)
+            console.log(getPokeColor(result.data.species.url))*/
+
             setPokeList(
                 state =>{
-                    state=[...state, result.data]
+                    state=[...state, result.data ]
                     state.sort( (a,b) => a.id > b.id ? 1 : -1 )
                     return state
                 }
             );
-        }) )
+        })
+            .catch(function (error) {
+                console.log(error);
+            }))
     }
+/*    const getPokeColor = async (p) => {
+        return await axios.get(p).then(result => {
+            result.data.color
+        })
+    }*/
 
     return (
         <>
-            <PokemonList pokeList={pokeList} color = {color} />
+            <PokemonListItem pokeList={pokeList} />
+            <Pagination />
         </>
-
     );
 }
 
