b2a2dd9a081b08b8c8c35af4b85feb252ca45ded
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-14 10:44:32 +0100

    fix id position

--- frontend/src/components/pokemonData/PokemonData.js
+++ frontend/src/components/pokemonData/PokemonData.js
@@ -5,6 +5,9 @@ import "../pokemonList/PokemonList.css";
 function PokemonData(props) {
     const setShow = props.setShow
     const pokeData = props.pokeData
+    console.log(pokeData)
+
+    // add stats (hp, speed, attack, defense),  types
 
     const closeModal = () => {
         setShow(false);
@@ -21,9 +24,13 @@ function PokemonData(props) {
                     <div className={"modal-image"}><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png`} alt=""/></div>
                 </div>
                 <div className={"modal-attributes"}>
-                    <div className={"modal-name"}>{pokeData.name.toUpperCase()}</div><br/>
-                    <div>BASE ATTRIBUTES</div> <br/>
+                    <div className={"modal-name"}>{pokeData.name.toUpperCase()}</div>
+                    <div className="modal-id">#{pokeData.id.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})}</div>
+                    <br/>
+                    <div>BASE STATISTICS</div> <br/>
                     <div className={"modal-text"}>XP: {pokeData.base_experience}</div>
+                    <div className={"modal-text"}>Height: {pokeData.height/10} m</div>
+                    <div className={"modal-text"}>Weight: {pokeData.weight/10} kg</div>
                 </div>
             </div>
         </div>
