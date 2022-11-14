25e8cc7f62354bff067e82fef17d88e0fdb5abd4
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-14 10:22:23 +0100

    fix image and close-modal position

--- frontend/src/components/pokemonData/PokemonData.js
+++ frontend/src/components/pokemonData/PokemonData.js
@@ -15,14 +15,16 @@ function PokemonData(props) {
     return (
         <div className={"modal-backdrop"} onClick={closeModal}>
             <div className={"modal-container"} onClick={e => e.stopPropagation()}>
-
-                <div className={`"modal-image-container" ${pokeData.color}`}>
-                    <div className={"close-modal"} onClick={closeModal}>X</div>
+                <div className={`stripe ${pokeData.color}`} ></div>
+                <div className={"close-modal"} onClick={closeModal}>X</div>
+                <div className={"modal-image-container"}>
                     <div className={"modal-image"}><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png`} alt=""/></div>
                 </div>
-                <div className={"modal-name"}>{pokeData.name.toUpperCase()}</div><br/>
-                <div className={"modal-text"}>ID: #{pokeData.id.toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})}</div>
-                <div className={"modal-text"}>Base experience: {pokeData.base_experience}</div>
+                <div className={"modal-attributes"}>
+                    <div className={"modal-name"}>{pokeData.name.toUpperCase()}</div><br/>
+                    <div>BASE ATTRIBUTES</div> <br/>
+                    <div className={"modal-text"}>XP: {pokeData.base_experience}</div>
+                </div>
             </div>
         </div>
     );
