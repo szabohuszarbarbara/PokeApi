4b8851f70e1b13432f4bcca1510779a3d6493540
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-21 10:26:06 +0100

    initialize Spring Boot

---
+++ backend/src/main/java/com/szhb/pokeapi/PokeApi.java
@@ -0,0 +1,13 @@
+package com.pokeapi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApi.class, args);
+	}
+
+}
