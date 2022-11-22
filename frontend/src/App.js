0e55d0f117c7d4d8d558a96100ed37233d35825c
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-22 17:30:37 +0100

    add context for user authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import Home from "./pages/Home";
 import "./App.css"
+import {AuthProvider} from "./context/authContext";
 
 function App() {
+    let user = localStorage.getItem("user");
+    user = JSON.parse(user);
   return (
     <>
-      <Home />
+        <AuthProvider userData={user}>
+            <Home />
+        </AuthProvider>
     </>
   );
 }
