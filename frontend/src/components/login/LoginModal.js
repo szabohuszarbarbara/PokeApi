55015ebe76abd6f525f1f8932eb12712392a0a87
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-11-30 10:17:14 +0100

    add register option to LoginModal.js

--- frontend/src/components/login/LoginModal.js
+++ frontend/src/components/login/LoginModal.js
@@ -1,6 +1,5 @@
 import {useContext, useState} from "react";
 import "../pokemonData/CardModal.css"
-import Pikachu from"../img/pikachu.jpg";
 import AuthContext from "../../context/authContext";
 
 function LoginModal(props) {
@@ -8,6 +7,10 @@ function LoginModal(props) {
     let {setUser} = useContext(AuthContext);
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState("");
+    const [state, setState] = useState("login")
 
     const closeModal = () => {
         setShow(false);
@@ -24,7 +27,29 @@ function LoginModal(props) {
             localStorage.setItem("user", JSON.stringify(user));
             setUser(user);
             closeModal();
+        } else {setError("Wrong username or password given!")}
+    }
+
+    const clickRegister = async (e) => {
+        e.preventDefault();
+        const payload = {
+            'username': username,
+            'password': password,
+            'email': email
         }
+        const registeredUser = await getUserData('/api/users/register', payload);
+        if (registeredUser) {
+            await clickLogin(e);
+        }
+    }
+
+    const validatePassword = (e) => {
+        if (password !== validatePassword) {
+            setError("Password and Confirm Password does not match.");
+        } else {
+            setError("");
+        }
+
     }
 
     const getUserData = async (url, payload) => {
@@ -37,10 +62,17 @@ function LoginModal(props) {
         })
         return await response.json();}
 
-    if (!props.show) { return null; }
+    const changeToRegister = () => {
+        setState("register")
+    }
+    const changeToLogin = () => {
+        setState("login")
+    }
 
+    if (!props.show) { return null; }
+    if (state === "login") {
     return (
-        <div className={"modal-backdrop"} onClick={closeModal}>
+        <div className={"modal-backdrop"} >
             <div className={"modal-container"} onClick={e => e.stopPropagation()}>
                 <div className={`stripe "red"`} ></div>
                 <div className={"close-modal"} onClick={closeModal}>X</div>
@@ -48,22 +80,70 @@ function LoginModal(props) {
                     <form className='modalForm' onSubmit={clickLogin}>
                         <h1 className='loginWelcomeText'>Welcome Back!</h1>
                         <div className='inputBox'>
-                            <input type="text" required="required" onChange={ e => setUserName(e.target.value)} value={username}/>
                             <span>Username</span>
+                            <input type="text" required="required" onChange={ e => setUserName(e.target.value)} value={username}/>
                             <i></i>
                         </div>
+                        <br/>
                         <div className='inputBox'>
-                            <input type="password" required="required" onChange={e => setPassword(e.target.value)} value={password}/>
                             <span>Password</span>
+                            <input type="password" required="required" onChange={e => setPassword(e.target.value)} value={password}/>
                             <i></i>
                         </div>
+                        <br/>
+                        <span className="error">{error}</span>
+                        <br/>
                         <button className='modalButton' type='submit' >Log In</button>
                     </form>
-
+                    <li className="item" onClick={changeToRegister}>Don't have an account? Register here... </li>
             </div>
         </div>
 
-    );
+    );}
+    else {
+        return (
+            <div className={"modal-backdrop"} >
+                <div className={"modal-container"} onClick={e => e.stopPropagation()}>
+                    <div className={`stripe "red"`} ></div>
+                    <div className={"close-modal"} onClick={closeModal}>X</div>
+
+                    <form className='modalForm' onSubmit={clickRegister}>
+                        <h1 className='loginWelcomeText'>Register</h1>
+                        <div className='inputBox'>
+                            <span>Username</span>
+                            <input type="text" required="required" onChange={ e => setUserName(e.target.value)} value={username}/>
+                            <i></i>
+                        </div>
+                        <br/>
+                        <div className='inputBox'>
+                            <span>E-mail</span>
+                            <input type="email" required="required" onChange={ e => setEmail(e.target.value)} value={email}/>
+                            <i></i>
+                        </div>
+                        <br/>
+                        <div className='inputBox'>
+                            <span>Password</span>
+                            <input type="password" required="required" onChange={e => setPassword(e.target.value)} value={password}/>
+                            <i></i>
+                        </div>
+                        <br/>
+                        <div className='inputBox'>
+                            <span>Re-enter password</span>
+                            <input type="password" required="required" onChange={e => setConfirmPassword(e.target.value)}
+                                   onBlur={validatePassword} value={confirmPassword}/>
+                            <i></i>
+                        </div>
+                        <br/>
+                        <span className="error">{error}</span>
+                        <br/>
+                        <button className='modalButton' type='submit' >Register</button>
+                    </form>
+                    <li className="item" onClick={changeToLogin}>Go back to login</li>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default LoginModal;
