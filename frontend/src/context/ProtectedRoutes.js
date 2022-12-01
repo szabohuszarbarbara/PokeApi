7bc49dd2fb245ddebf2d7db562abeaa59a7701d9
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-12-01 08:35:30 +0100

    add Home component as the background to P rotectedRoutes

--- frontend/src/context/ProtectedRoutes.js
+++ frontend/src/context/ProtectedRoutes.js
@@ -1,12 +1,16 @@
 import AuthContext from "./authContext";
 import {useContext} from "react";
 import LoginModal from "../components/login/LoginModal";
+import Home from "../pages/Home";
 
 const ProtectedRoute = ({children, allowedRole }) => {
     let { user } = useContext(AuthContext);
 
     if (!user) {
-        return <LoginModal show={true}/>;
+        return <>
+            <Home />
+            <LoginModal show={true}/>;
+            </>
         }
     else {
         return children;
