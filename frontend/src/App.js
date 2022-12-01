0f803af7e5a97fd1d89276d57b8987deb5591a7e
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-12-01 08:34:00 +0100

    add /home to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home/>} />
+                    <Route path="/home" element={<Home/>} />
                     <Route path="/profile" element= {<ProtectedRoute allowedRole="ROLE_ADMIN"><Profile /> </ProtectedRoute >} />
                 </Routes>
             </BrowserRouter>
