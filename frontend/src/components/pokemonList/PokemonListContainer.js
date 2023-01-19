a79baf1ae162299b8c4259d0dc8bc0afc0b36be7
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2023-01-19 14:47:29 +0100

    change the source of the fetch from pokemon api to java server

--- frontend/src/components/pokemonList/PokemonListContainer.js
+++ frontend/src/components/pokemonList/PokemonListContainer.js
@@ -5,7 +5,7 @@ import Pagination from "../pagination/Pagination";
 
 function PokemonListContainer() {
     const [pokeList, setPokeList] = useState([]);
-    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+    const [url, setUrl] = useState("/api/pokemon")
     const [prevUrl, setPrevUrl] = useState("")
     const [nextUrl, setNextUrl] = useState("")
 
@@ -15,46 +15,13 @@ function PokemonListContainer() {
     }, [url])
 
     const getData = () => {
-        axios.get(url).then(result => {
-            setPrevUrl(result.data.previous)
-            setNextUrl(result.data.next)
-            const resultList = result.data.results
-            const urls = resultList.map(p => p.url)
-            getPokeList(urls);
-/*            localStorage.setItem("data", JSON.stringify(pokeList))*/
+        axios.get(url).then(result => {console.log(result)
         })
             .catch(function (error) {
                 console.log(error);
             })
     }
 
-    const getPokeList = (urls) => {
-        urls.map( async url => await axios.get(url).then(async result => {
-
-            const pData = result.data
-            pData.color = await getPokeColor(result.data.species.url)
-
-            setPokeList(
-                state => {
-                    state = [...state, pData]
-                    state.sort((a, b) => a.id > b.id ? 1 : -1)
-                    return state
-                }
-            );
-        })
-            .catch(function (error) {
-                console.log(error);
-            }))
-    }
-
-    const getPokeColor = async (url) => {
-        try {
-            const result = await axios.get(url)
-            return result.data.color.name
-        } catch(error) {
-            return error
-        }
-    }
     const goToPrevPage = () => {
         setUrl(prevUrl)
     }
